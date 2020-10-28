ebe42e1f00da20b6da39ed53d04a21e848dea01d
André Luiz <andre.andrade@vsoft.com.br>
2020-10-28 09:22:04 -0300

    Update pom.xml

--- target/generated-sources/annotations/br/com/andre/msscbeerorderservice/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/br/com/andre/msscbeerorderservice/web/mappers/BeerOrderLineMapperImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-10-12T16:14:59-0300",
-    comments = "version: 1.4.0.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
+    date = "2020-10-28T09:17:53-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
 @Primary
