15355a180fb1b53d00ad50d848a55b8b59e54a23
André Luiz <andre.andrade@vsoft.com.br>
2020-11-02 15:14:49 -0300

    Configuring local mysql spring profile.

--- src/main/java/br/com/andre/msscbeerorderservice/domain/Customer.java
+++ src/main/java/br/com/andre/msscbeerorderservice/domain/Customer.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -29,7 +30,8 @@ public class Customer extends BaseEntity {
 
     private String customerName;
 
-    @Column(length = 36, columnDefinition = "varchar")
+    @Type(type="org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)")
     private UUID apiKey;
 
     @OneToMany(mappedBy = "customer")
