311584c41b5bccbfa8fdc91e00374a609bbd04bc
André Luiz <andre.andrade@vsoft.com.br>
2020-11-09 06:26:04 -0300

    Setting Allocation State Machine States. Closes #9

---
+++ src/main/java/br/com/andre/msscbeerorderservice/sm/BeerOrderStateMachineConfig.java
@@ -0,0 +1,26 @@
+package br.com.andre.msscbeerorderservice.sm;
+
+import br.com.andre.msscbeerorderservice.domain.BeerOrderStatusEnum;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.statemachine.config.EnableStateMachineFactory;
+import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
+import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+
+import java.util.EnumSet;
+
+@Configuration
+@EnableStateMachineFactory
+public class BeerOrderStateMachineConfig extends StateMachineConfigurerAdapter<BeerOrderStatusEnum, BeerOrderStatusEnum> {
+
+    @Override
+    public void configure(StateMachineStateConfigurer<BeerOrderStatusEnum, BeerOrderStatusEnum> states) throws Exception {
+        states.withStates()
+                .initial(BeerOrderStatusEnum.NEW)
+                .states(EnumSet.allOf(BeerOrderStatusEnum.class))
+                .end(BeerOrderStatusEnum.PICKED_UP)
+                .end(BeerOrderStatusEnum.DELIVERED)
+                .end(BeerOrderStatusEnum.DELIVERY_EXCEPTION)
+                .end(BeerOrderStatusEnum.VALIDATION_EXCEPTION)
+                .end(BeerOrderStatusEnum.ALLOCATED_EXCEPTION);
+    }
+}
