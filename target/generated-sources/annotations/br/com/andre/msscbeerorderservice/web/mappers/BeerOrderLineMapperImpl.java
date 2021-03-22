aeeb2177fbc8f15998fe4604c9a3502dd619704c
André Luiz <andre.andrade@vsoft.com.br>
2021-03-22 18:13:11 -0300

    .

--- target/generated-sources/annotations/br/com/andre/msscbeerorderservice/web/mappers/BeerOrderLineMapperImpl.java
+++ target/generated-sources/annotations/br/com/andre/msscbeerorderservice/web/mappers/BeerOrderLineMapperImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2021-02-16T08:54:32-0300",
+    date = "2021-03-22T12:53:17-0300",
     comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.8 (Oracle Corporation)"
 )
 @Component
